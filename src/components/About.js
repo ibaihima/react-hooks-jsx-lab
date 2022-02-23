684bf88b88d365754a04200fb5f739ef2388867d
Ibaihima <ibaihima@hotmail.com>
2022-02-23 13:11:02 -0500

    commit tree e3cf7d498774e66ba1e892fa4a024a98bb9c0aa6

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,19 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id= "about">
+    <h2>
+      About Me
+    </h2>
+    <p>
+      this is about meee i guess
+    </p>
+    <img
+    src= {image} alt = "I made this"
+    />
+  </div>
+  );
 }
 
 export default About;
